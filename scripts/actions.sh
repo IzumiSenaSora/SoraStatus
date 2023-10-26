494f58e6c3d9342b906491a528fa328e66b9c07b
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2023-10-27 00:59:40 +0600

    Update SORASTATUS Fri Oct 27 00:59:40 +06 2023
    Changes:
    
    M  scripts/actions.sh
    
    Signed-off-by: Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -2,9 +2,11 @@
 
 if [[ "$CI" = "true" ]]; then
 
+	CURRENT="$(git rev-parse HEAD)"
 	BRANCH="$(git branch --show-current)"
 	REPO="$(basename -s .git "$(git remote get-url origin)" | tr '[:upper:]' '[:lower:]')"
 
+	export CURRENT
 	export BRANCH
 	export REPO
 
@@ -86,114 +88,121 @@ $(git status --short)" || true
 		git status --short
 	fi
 
-	if [[ -d static ]]; then
+	LATEST="$(git rev-parse origin/"$BRANCH")"
 
-		if [[ "$BRANCH" = "staging" ]]; then
-			git reset --hard
+	export LATEST
+
+	if [[ "CURRENT" != "$LATEST" ]]; then
+
+		if [[ -d static ]]; then
+
+			if [[ "$BRANCH" = "staging" ]]; then
+				git reset --hard
+			fi
+
+			replace() {
+
+				find . -type f '(' -name "*.html" -o -name "*.css" -o -name "*.js" ')' | while read -r FILE; do
+
+					sed -i "s%https://aeonquake.eu.org%https://staging.aeonquake.eu.org%g" "$FILE"
+					sed -i "s%https://lotns.eu.org%https://staging.lotns.eu.org%g" "$FILE"
+					sed -i "s%https://notryan.eu.org%https://staging.notryan.eu.org%g" "$FILE"
+					sed -i "s%https://soraapis.eu.org%https://staging.soraapis.eu.org%g" "$FILE"
+					sed -i "s%https://soracdns.eu.org%https://staging.soracdns.eu.org%g" "$FILE"
+					sed -i "s%https://sorablog.eu.org%https://staging.sorablog.eu.org%g" "$FILE"
+					sed -i "s%https://about.soracloud.eu.org%https://staging.about.soracloud.eu.org%g" "$FILE"
+					sed -i "s%https://soradns.eu.org%https://staging.soradns.eu.org%g" "$FILE"
+					sed -i "s%https://sorafonts.eu.org%https://staging.sorafonts.eu.org%g" "$FILE"
+					sed -i "s%https://soralicense.eu.org%https://staging.soralicense.eu.org%g" "$FILE"
+					sed -i "s%https://sorastatus.eu.org%https://staging.sorastatus.eu.org%g" "$FILE"
+					sed -i "s%https://unordinary.eu.org%https://staging.unordinary.eu.org%g" "$FILE"
+				done
+			}
+
+			if [[ "$REPO" = "aeonquake" ]]; then
+				export VHOST="aeonquake-unordinary.vercel.app"
+				export HOST="staging.aeonquake.eu.org"
+
+			elif [[ "$REPO" = "lotns" ]]; then
+				export VHOST="lotns-unordinary.vercel.app"
+				export HOST="staging.lotns.eu.org"
+
+			elif [[ "$REPO" = "notryan" ]]; then
+				export VHOST="notryan-unordinary.vercel.app"
+				export HOST="staging.notryan.eu.org"
+
+			elif [[ "$REPO" = "soraapis" ]]; then
+				export VHOST="soraapis-unordinary.vercel.app"
+				export HOST="staging.soraapis.eu.org"
+
+			elif [[ "$REPO" = "sorablog" ]]; then
+				export VHOST="sorablog-unordinary.vercel.app"
+				export HOST="staging.sorablog.eu.org"
+
+			elif [[ "$REPO" = "soracdns" ]]; then
+				export VHOST="soracdns-unordinary.vercel.app"
+				export HOST="staging.soracdns.eu.org"
+
+			elif [[ "$REPO" = "about_soracloud" ]]; then
+				export VHOST="about-soracloud-unordinary.vercel.app"
+				export HOST="staging.about.soracloud.eu.org"
+
+			elif [[ "$REPO" = "soradns" ]]; then
+				export VHOST="soradns-unordinary.vercel.app"
+				export HOST="staging.soradns.eu.org"
+
+			elif [[ "$REPO" = "sorafonts" ]]; then
+				export VHOST="sorafonts-unordinary.vercel.app"
+				export HOST="staging.sorafonts.eu.org"
+
+			elif [[ "$REPO" = "soralicense" ]]; then
+				export VHOST="soralicense-unordinary.vercel.app"
+				export HOST="staging.soralicense.eu.org"
+
+			elif [[ "$REPO" = "sorastatus" ]]; then
+				export VHOST="sorastatus-unordinary.vercel.app"
+				export HOST="staging.sorastatus.eu.org"
+
+			elif [[ "$REPO" = "unordinary" ]]; then
+				export VHOST="unordinary-unordinary.vercel.app"
+				export HOST="staging.unordinary.eu.org"
+			fi
+
+			source .env
+
+			if ! command -v vercel >/dev/null 2>&1; then
+				npm install --global vercel@latest >/dev/null 2>&1
+			fi
+
+			cd "$GITHUB_WORKSPACE"/static || exit
+
+			if [[ "$BRANCH" = "main" ]]; then
+				vercel pull --yes --environment=production --token "$VERCEL_TOKEN"
+				vercel build --prod --token "$VERCEL_TOKEN"
+				vercel deploy --prebuilt --prod --token "$VERCEL_TOKEN"
+			else
+				replace
+				vercel pull --yes --environment=preview --token "$VERCEL_TOKEN"
+				vercel build --token "$VERCEL_TOKEN"
+				vercel deploy --prebuilt --token "$VERCEL_TOKEN"
+				vercel alias set "$VHOST" "$HOST" --token "$VERCEL_TOKEN"
+			fi
+
+			rm -rf "$GITHUB_WORKSPACE"/static/.vercel/
+
+			if ! command -v netlify >/dev/null 2>&1; then
+				npm install --global netlify-cli >/dev/null 2>&1
+			fi
+
+			cd "$GITHUB_WORKSPACE" || exit
+
+			if [[ "$BRANCH" = "main" ]]; then
+				netlify deploy --dir="static" --prod
+			else
+				replace
+				netlify deploy --dir="static"
+			fi
+
 		fi
-
-		replace() {
-
-			find . -type f '(' -name "*.html" -o -name "*.css" -o -name "*.js" ')' | while read -r FILE; do
-
-				sed -i "s%https://aeonquake.eu.org%https://staging.aeonquake.eu.org%g" "$FILE"
-				sed -i "s%https://lotns.eu.org%https://staging.lotns.eu.org%g" "$FILE"
-				sed -i "s%https://notryan.eu.org%https://staging.notryan.eu.org%g" "$FILE"
-				sed -i "s%https://soraapis.eu.org%https://staging.soraapis.eu.org%g" "$FILE"
-				sed -i "s%https://soracdns.eu.org%https://staging.soracdns.eu.org%g" "$FILE"
-				sed -i "s%https://sorablog.eu.org%https://staging.sorablog.eu.org%g" "$FILE"
-				sed -i "s%https://about.soracloud.eu.org%https://staging.about.soracloud.eu.org%g" "$FILE"
-				sed -i "s%https://soradns.eu.org%https://staging.soradns.eu.org%g" "$FILE"
-				sed -i "s%https://sorafonts.eu.org%https://staging.sorafonts.eu.org%g" "$FILE"
-				sed -i "s%https://soralicense.eu.org%https://staging.soralicense.eu.org%g" "$FILE"
-				sed -i "s%https://sorastatus.eu.org%https://staging.sorastatus.eu.org%g" "$FILE"
-				sed -i "s%https://unordinary.eu.org%https://staging.unordinary.eu.org%g" "$FILE"
-			done
-		}
-
-		if [[ "$REPO" = "aeonquake" ]]; then
-			export VHOST="aeonquake-unordinary.vercel.app"
-			export HOST="staging.aeonquake.eu.org"
-
-		elif [[ "$REPO" = "lotns" ]]; then
-			export VHOST="lotns-unordinary.vercel.app"
-			export HOST="staging.lotns.eu.org"
-
-		elif [[ "$REPO" = "notryan" ]]; then
-			export VHOST="notryan-unordinary.vercel.app"
-			export HOST="staging.notryan.eu.org"
-
-		elif [[ "$REPO" = "soraapis" ]]; then
-			export VHOST="soraapis-unordinary.vercel.app"
-			export HOST="staging.soraapis.eu.org"
-
-		elif [[ "$REPO" = "sorablog" ]]; then
-			export VHOST="sorablog-unordinary.vercel.app"
-			export HOST="staging.sorablog.eu.org"
-
-		elif [[ "$REPO" = "soracdns" ]]; then
-			export VHOST="soracdns-unordinary.vercel.app"
-			export HOST="staging.soracdns.eu.org"
-
-		elif [[ "$REPO" = "about_soracloud" ]]; then
-			export VHOST="about-soracloud-unordinary.vercel.app"
-			export HOST="staging.about.soracloud.eu.org"
-
-		elif [[ "$REPO" = "soradns" ]]; then
-			export VHOST="soradns-unordinary.vercel.app"
-			export HOST="staging.soradns.eu.org"
-
-		elif [[ "$REPO" = "sorafonts" ]]; then
-			export VHOST="sorafonts-unordinary.vercel.app"
-			export HOST="staging.sorafonts.eu.org"
-
-		elif [[ "$REPO" = "soralicense" ]]; then
-			export VHOST="soralicense-unordinary.vercel.app"
-			export HOST="staging.soralicense.eu.org"
-
-		elif [[ "$REPO" = "sorastatus" ]]; then
-			export VHOST="sorastatus-unordinary.vercel.app"
-			export HOST="staging.sorastatus.eu.org"
-
-		elif [[ "$REPO" = "unordinary" ]]; then
-			export VHOST="unordinary-unordinary.vercel.app"
-			export HOST="staging.unordinary.eu.org"
-		fi
-
-		source .env
-
-		if ! command -v vercel >/dev/null 2>&1; then
-			npm install --global vercel@latest >/dev/null 2>&1
-		fi
-
-		cd "$GITHUB_WORKSPACE"/static || exit
-
-		if [[ "$BRANCH" = "main" ]]; then
-			vercel pull --yes --environment=production --token "$VERCEL_TOKEN"
-			vercel build --prod --token "$VERCEL_TOKEN"
-			vercel deploy --prebuilt --prod --token "$VERCEL_TOKEN"
-		else
-			replace
-			vercel pull --yes --environment=preview --token "$VERCEL_TOKEN"
-			vercel build --token "$VERCEL_TOKEN"
-			vercel deploy --prebuilt --token "$VERCEL_TOKEN"
-			vercel alias set "$VHOST" "$HOST" --token "$VERCEL_TOKEN"
-		fi
-
-		rm -rf "$GITHUB_WORKSPACE"/static/.vercel/
-
-		if ! command -v netlify >/dev/null 2>&1; then
-			npm install --global netlify-cli >/dev/null 2>&1
-		fi
-
-		cd "$GITHUB_WORKSPACE" || exit
-
-		if [[ "$BRANCH" = "main" ]]; then
-			netlify deploy --dir="static" --prod
-		else
-			replace
-			netlify deploy --dir="static"
-		fi
-
 	fi
 fi
